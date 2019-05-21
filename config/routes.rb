f5e5da6d29c8bd419daf7fc78fa6cf6b7916b3bc
Tara Sankhalpara <tsankhalpara@mac.com>
2019-05-21 16:53:48 +0100

    create user model and add views and form for signing up
    co-authored-by: Michael <darmodymichael@gmail.com>
    co-authored-by: Alasdair <alasdair123@hotmail.co.uk>

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :users, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+
+  get '/signup', to: 'users#new'
 end

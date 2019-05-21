5182ffe8b2e9badec638b5a6d0a2e76515f68b4d
Tara Sankhalpara <tsankhalpara@mac.com>
2019-05-21 11:43:43 +0100

    Allow users to edit posts
    Co-authored-by: Michael <darmodymichael@gmail.com>

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,16 @@ class PostsController < ApplicationController
     redirect_to posts_url
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update_attributes(post_params)
+    redirect_to posts_path
+  end
+
   private
 
   def post_params

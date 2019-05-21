c2209fa8501f5453dd6bdb1cc4bdf8fc818e721b
Tara Sankhalpara <tsankhalpara@mac.com>
2019-05-21 10:52:46 +0100

    allow users to delete posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,6 +12,11 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def destroy
+    Post.find(params[:id]).destroy
+    redirect_to posts_url
+  end
+
   private
 
   def post_params

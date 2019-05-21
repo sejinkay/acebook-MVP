f0021c0e374f6e0d4a526ad371fec4c21130cc82
Tara Sankhalpara <tsankhalpara@mac.com>
2019-05-21 12:18:21 +0100

    remove additional routes from file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'posts/:id/edit', to: 'posts#edit', as: :edit_posts
-  patch 'posts/:id', to: 'posts#update'
   resources :posts, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 end

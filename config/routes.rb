90977ceac018ae9d2d69e301584ba5272a815cc9
Tara Sankhalpara <tsankhalpara@mac.com>
2019-05-22 14:49:42 +0100

    Enable sessions to be created when you login. co-authored-by: Simon <yichenbo@outlook.com> co-authored-by: Frankie <frankie4@btinternet.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  get 'login', to: 'sessions#new', as: 'login'
 
   get 'sessions/create'
 
-  get 'sessions/destroy'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   resources :users, only: [:index, :show, :new, :create, :edit, :update, :destroy]
-
+  resources :sessions, only: [:new, :create, :destroy]
   root 'users#new'
 end

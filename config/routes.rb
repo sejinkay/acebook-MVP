e67f462c8658b226ba4cf0924c7e19766927053a
Tara Sankhalpara <tsankhalpara@mac.com>
2019-05-22 12:26:34 +0100

    change root view file and add log in form co-authored-by: Simon <yichenbo@outlook.com> co-authored-by: Frankie <frankie4@btinternet.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   resources :users, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+
+  root 'users#new'
 end

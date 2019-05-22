b01de6252a34d20aa5608231cab7d931c075032f
FrankieBoye <frankie4@btinternet.com>
2019-05-22 15:49:18 +0100

    testing login authentication co-authored-by: Tara <tsankhalpara@mac.com> co-authored-by: Simon <yichenbo@outlook.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,12 +7,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
-      if user && user.authenticate(params[:password])
-        session[:user_id] = user.id
+    p params
+    p (params[:email])
+    p user = User.find_by(email: params[:session][:email])
+      if user && user.authenticate(params[:session][:password])
+        log_in user
         redirect_to posts_url, notice: "Logged in!"
       else
-        flash.now[:alert] = "Email or password is invalid"
+        flash.now[:danger] = "Email or password is invalid"
         render "new"
       end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'login', to: 'sessions#new', as: 'login'
 
+  post '/login', to: 'sessions#create'
+
   get 'sessions/create'
 
   get 'logout', to: 'sessions#destroy', as: 'logout'

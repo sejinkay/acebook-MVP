0e8afc4d82f698bb437fa05a1b91976be55c07a5
FrankieBoye <frankie4@btinternet.com>
2019-05-23 11:05:38 +0100

    changed routes and generated a session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+
+  get'/' => 'users#new'
+  post '/users' => 'users#create'
+  get 'login', to: 'sessions#new', as: 'login'
+  post '/login' => 'sessions#create'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts, only: [:index, :show, :new, :create, :edit, :update, :destroy]
-  resources :users, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  # resources :users, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 
-  root "users#new"
+  # root "users#new"
+
 end

51d5f498b293bbf0a843508fd4ce4ae3d8dc43f8
Chenbo Yi <yichenbo@outlook.com>
2019-05-28 11:52:22 +0100

    update the post table to have wall_owner_id
    co-authored-by: Alasdair Wem <alasdair123@hotmi
    co-authored-by: Alasdair Wem <alasdair123@hotmail.co.uk>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190523134027) do
+ActiveRecord::Schema.define(version: 20190528104028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,13 +20,14 @@ ActiveRecord::Schema.define(version: 20190523134027) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "users_id"
+    t.integer "wall_owner_id"
     t.index ["users_id"], name: "index_posts_on_users_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.text "email"
-    t.text "name"
-    t.text "password"
+    t.string "email"
+    t.string "name"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
